0ba4ef85518df8a81a781e68e924534b6b68374f
Yeongjae-Kong <kong7300@dgist.ac.kr>
2024-01-24 06:02:32 +0900

    modify fin

--- src/components/page1.tsx
+++ src/components/page1.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { a } from '@react-spring/web'
+import './page1.css'
 
 export default function Page1({ fill }: {fill: any}) {
   // Just a Figma export, the fill is animated
@@ -7,23 +8,27 @@ export default function Page1({ fill }: {fill: any}) {
     <div className="overlay">
       <a.svg viewBox="0 0 583 720" fill={fill} xmlns="http://www.w3.org/2000/svg">
         <text style={{ whiteSpace: 'pre' }} fontFamily="Inter" fontSize={11} fontWeight={500} letterSpacing="0em">
-          <tspan x={40} y={188.318} children="Major in Computer Science, DGIST" />
+          <tspan x={40} y={168.318} children="Major in Computer Science, DGIST" />
         </text>
-        <text fill="#E8B059" style={{ whiteSpace: 'pre' }} fontFamily="Inter" fontSize={52} fontWeight="bold" letterSpacing="0em">
-          <tspan x={40} y={257.909} children={'3D Portfolio'} />
+        <text fill="#E8B059" style={{ whiteSpace: 'pre' }} fontFamily="Inter" fontSize={60} fontWeight="bold" letterSpacing="0em">
+          <tspan x={40} y={237.909} children={'3D Portfolio'} />
         </text>
         <text style={{ whiteSpace: 'pre' }} fontFamily="Inter" fontSize={12} fontWeight="bold" letterSpacing="0em">
-          <tspan x={40} y={270.909} />
+          <tspan x={40} y={250.909} />
         </text>
-        <text style={{ whiteSpace: 'pre' }} fontFamily="Inter" fontSize={40} fontWeight="bold" letterSpacing="0em">
-          <tspan x={40} y={321.909} children="Hi, my" />
-          <tspan x={40} y={372.909} children="name is Yeongjae-Kong." />
-          <tspan x={40} y={423.909} children="I'm Full-stack developer." />
-          <tspan x={40} y={474.909} children="Welcome to my website!" />
+        <text style={{ whiteSpace: 'pre' }} fontFamily="Inter" fontSize={34} fontWeight="bold" letterSpacing="0em">
+          <tspan x={40} y={301.909} children="Hi, my" />
+          <tspan x={40} y={352.909} children="name is Yeongjae-Kong." />
+          <tspan x={40} y={403.909} children="I'm Full-stack developer." />
+          <tspan x={40} y={454.909} children="Welcome to my website!" />
         </text>
-        <text style={{ whiteSpace: 'pre' }} fontFamily="Inter" fontSize={11} fontWeight={500} letterSpacing="0em">
+        {/* <text style={{ whiteSpace: 'pre' }} fontFamily="Inter" fontSize={11} fontWeight={500} letterSpacing="0em">
           <tspan x={366} y={640.318} children="Develop with haejune-jung" />
-        </text>
+        </text> */}
+        <div className="glassmorphism-container">
+          <p>GitHub: [여기에 GitHub 주소]</p>
+          <p>Contact: [여기에 Gmail 주소]</p>
+        </div>
       </a.svg>
     </div>
   )
